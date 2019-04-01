5896a9d1bdd3982c5eae552d1e5f537bf83da70f
Siddarth <siddarthrgovindan@gmail.com>
2019-04-01 14:19:24 -0400

    Writing some CONSTRUCTORS >:O for QANode.cs so many constructors

--- CodeLabW7/Assets/Scripts/QAManager.cs
+++ CodeLabW7/Assets/Scripts/QAManager.cs
@@ -1,24 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QAManager : MonoBehaviour
 {
-    private QANode currentNode;
+    public Text questionText;
+    public Text option1Text;
+    public Text option2Text;
+    
+    QANode currentNode;
     
     // Start is called before the first frame update
     void Start()
     {
         currentNode = new QANode();
-
+        
+        currentNode.pageNumber = 1;
         currentNode.questionText = "You come to a fork in the road.\n Do you go left or right?";
         currentNode.option1Text = "Left";
         currentNode.option2Text = "Right";
+        UpdateUI(currentNode);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void UpdateUI(QANode node)
+    {
+        questionText.text = node.questionText;
+        option1Text.text = node.option1Text;
+        option2Text.text = node.option2Text;
     }
 }
--- CodeLabW7/Assets/Scripts/QANode.cs
+++ CodeLabW7/Assets/Scripts/QANode.cs
@@ -3,7 +3,30 @@ using System.Collections.Generic;
 
 public class QANode
 {
+    public int pageNumber;
     public string questionText;
     public string option1Text;
     public string option2Text;
+
+    public QANode() //default constructor is empty
+    {
+        pageNumber = 0;
+        questionText = "This is default question text. ?";
+        option1Text = "??";
+        option2Text = "!!";
+    }
+
+    public QANode(int pageNumber, string questionText) // you can have ANY number of constructors for a function!!!
+    {
+        this.pageNumber = pageNumber;
+        this.questionText = questionText;
+    }
+
+    public QANode(int pageNumber, string questionText,string option1Text, string option2Text)
+    {
+        this.pageNumber = pageNumber;
+        this.questionText = questionText;
+        this.option1Text = option1Text;
+        this.option2Text = option2Text;
+    }
 }
